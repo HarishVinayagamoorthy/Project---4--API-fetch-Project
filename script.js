0766a50e0f36ed00c799e827fcbdea1b5e8a233b
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2023-09-05 09:35:47 +0530

    updated

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 async function foo(){
+
+  try{
+
+
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
     const result= await res.json();
-    console.log(result)
+     // console.log(result)
 
     for(var i=0; i< result.length; i++){
         const div = document.createElement("div");
-        div.innerHTML=`
+        div.innerHTML=`<div class ="container-fluid" id="body-cards">
         <div class="row row-cols-1 row-cols-md-2 g-4" >
             <div class="col" id="cards">
               <div class="card">
@@ -18,8 +22,16 @@ async function foo(){
                   <p class="card-text">City: ${result[i].address. city}</p>
                 </div>
               </div>
+            </div>
             </div>`
         document.body.append(div);
     }
-}
+  }
+  catch(error){
+    console.log(error)
+  }
+  
+  }
+
+
 foo();
